50e811e47a9849e4f2505e65ea067a7049a332f3
Dawson Graf <38477934+dgraf5@users.noreply.github.com>
2024-05-08 02:47:01 -0700

    Add files via upload

---
+++ YoloV4 Model/OpenCV/opencv_data_config.hpp
@@ -0,0 +1,11 @@
+
+#define OPENCV_INSTALL_PREFIX "C:/OpenCV_Build/build2/install"
+
+#define OPENCV_DATA_INSTALL_PATH "etc"
+
+#define OPENCV_BUILD_DIR "C:/OpenCV_Build/build2"
+
+#define OPENCV_DATA_BUILD_DIR_SEARCH_PATHS \
+    "../opencv/sources/"
+
+#define OPENCV_INSTALL_DATA_DIR_RELATIVE "../../../etc"
